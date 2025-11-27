474e3e677c735a96259a1d7a53432c3728a3acb3
노미경 <codingworld@naver.com>
2025-11-27 13:54:46 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.kotlin"
+version = "0.0.1-SNAPSHOT"
+description = "aws-bucket"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:3.3.0")
+//    implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:4.0.0-M1")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/kotlin/aws/AwsBucketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.aws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsBucketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
